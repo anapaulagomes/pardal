ff104e2ac770396cd5a8f1e8365d3ae27a5c8d3f
Wellington Mitrut <wellingtonmitrut@gmail.com>
2019-03-05 16:30:55 -0300

    Added app icons and .DS_store to git ignore for future mac contributors (#31)
    * Added app icons and .DS_store to git ignore for future mac contributors
    
    * Review changes updated
    
    * changed breaking icon

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.on('ready', () => {
     mainWindow.hide();
   });
 
-  const tray = new Tray(`${__dirname}/app/img/temp.png`);
+  const tray = new Tray(`${__dirname}/app/img/icon.png`);
   const contextMenu = Menu.buildFromTemplate([
     {
       label: 'Show Pardal',
